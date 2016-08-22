7cc34fac82d72db6cd27530814aab650884d7222
ga <leimin_398056693@qq.com>
2016-08-22 19:14:40 +0800

    上传

--- app/src/main/java/com/neo/http/prettier/application/MyApplication.java
+++ app/src/main/java/com/neo/http/prettier/application/MyApplication.java
@@ -6,4 +6,8 @@ import android.app.Application;
  * Created by ga on 2016/8/22.
  */
 public class MyApplication extends Application {
+
+
+
+    
 }
--- app/src/main/java/com/neo/http/prettier/fragment/MeiGouFragment.java
+++ app/src/main/java/com/neo/http/prettier/fragment/MeiGouFragment.java
@@ -1,7 +1,7 @@
 package com.neo.http.prettier.fragment;
 
 
-import com.neo.http.prettier.R;
+
 import com.neo.http.utillibrary.base.BaseFragment;
 
 /**
@@ -12,7 +12,7 @@ public class MeiGouFragment extends BaseFragment {
 
     @Override
     protected int getContentView() {
-        return R.layout.fragment_meigou;
+        return 0;
     }
 }
 
